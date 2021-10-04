09927a47dcf93fc9ecbe5933e35edb6598647b2c
clevandowski <cyrille.levandowski@gmail.com>
2021-10-04 08:40:35 +0200

    check_internet - Le test est maintenant basé sur l'accès au port 53 du DNS de google (8.8.8.8) au lieu de faire un ping (icmp)

--- check_internet/check_internet.sh
+++ check_internet/check_internet.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if ping -c 1 -W 1 8.8.8.8 >/dev/null 2>&1; then
+if nc -z -w3 8.8.8.8 53 >/dev/null 2>&1; then
   echo "$(date +[%Y-%m-%d\ %H:%M:%S]) Internet access is up"
 fi
 while true; do
-  while ping -c 1 -W 1 8.8.8.8 >/dev/null 2>&1; do
+  while nc -z -w3 8.8.8.8 53 >/dev/null 2>&1; do
     sleep 1
   done
   echo "$(date +[%Y-%m-%d\ %H:%M:%S]) Internet access is down"
-  while ! ping -c 1 -W 1 8.8.8.8 >/dev/null 2>&1; do
+  while !  nc -z -w3 8.8.8.8 53 >/dev/null 2>&1; do
     sleep 1
   done
   echo "$(date +[%Y-%m-%d\ %H:%M:%S]) Internet access is up"
